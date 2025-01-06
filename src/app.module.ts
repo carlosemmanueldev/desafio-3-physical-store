0c677fecfe5dc6728eac507689cf41f8797bf9e9
Carlos <carlos.emmanuel.236@gmail.com>
2025-01-06 14:17:46 -0300

    Configurando conexao do bd e variaveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StoresModule } from './stores/stores.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('DATABASE'),
+      }),
+    }),
+    StoresModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
