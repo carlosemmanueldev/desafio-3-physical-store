e4e7299c4f9cd4e305eff02173a8a6966d5a5910
Carlos <carlos.emmanuel.236@gmail.com>
2025-01-10 05:49:34 -0300

    Adicionando novos modulos e o validation pipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StoresModule } from './stores/stores.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_PIPE } from '@nestjs/core';
+import { GoogleMapsService } from './google-maps/google-maps.service';
+import { GoogleMapsModule } from './google-maps/google-maps.module';
+import { ViaCepModule } from './via-cep/via-cep.module';
+import { CorreiosModule } from './correios/correios.module';
 
 @Module({
   imports: [
@@ -17,9 +22,21 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         uri: configService.get<string>('DATABASE'),
       }),
     }),
-    StoresModule
+    StoresModule,
+    GoogleMapsModule,
+    ViaCepModule,
+    CorreiosModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+    GoogleMapsService
+  ],
 })
 export class AppModule {}
