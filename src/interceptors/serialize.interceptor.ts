d05fea6b9e7e7172fd1060a11d0f8806b9ddd77f
Carlos <carlos.emmanuel.236@gmail.com>
2025-01-10 05:49:53 -0300

    Adicionando serializer

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,23 @@
+import { CallHandler, ExecutionContext, NestInterceptor, UseInterceptors } from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+import { plainToInstance } from 'class-transformer';
+
+interface ClassConstructor {
+  new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor): MethodDecorator & ClassDecorator {
+  return UseInterceptors(new SerializeInterceptor(dto));
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data: any) => {
+        return plainToInstance(this.dto, data, { excludeExtraneousValues: true });
+      }),
+    );
+  }
+}
