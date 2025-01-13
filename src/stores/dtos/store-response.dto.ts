7cd2792473ff896e9c6a8e62f68c41f443164a77
Carlos <carlos.emmanuel.236@gmail.com>
2025-01-13 06:03:24 -0300

    Adicionando store-response.dto.ts

---
+++ src/stores/dtos/store-response.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
+import { StoreDto } from './store.dto';
+
+export class StoreResponseDto {
+  @ApiProperty({
+    description: 'Response status',
+    example: 'success',
+  })
+  @Expose()
+  status: string;
+
+  @ApiProperty({
+    description: 'Details of the store',
+    type: StoreDto,
+  })
+  @Expose()
+  @Type(() => StoreDto)
+  store: StoreDto;
+}
