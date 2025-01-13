1835f1ad7d1f8fcf0fdd070350de86027daf2a05
Carlos <carlos.emmanuel.236@gmail.com>
2025-01-13 06:18:14 -0300

    Ajustando import de store no store.module.ts

--- src/stores/stores.module.ts
+++ src/stores/stores.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { StoresController } from './stores.controller';
 import { StoresService } from './stores.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Store, StoreSchema } from './store.schema';
+import { Store, StoreSchema } from './schemas/store.schema';
 import { ViaCepModule } from '../via-cep/via-cep.module';
 import { GoogleMapsModule } from '../google-maps/google-maps.module';
 import { CorreiosModule } from '../correios/correios.module';
