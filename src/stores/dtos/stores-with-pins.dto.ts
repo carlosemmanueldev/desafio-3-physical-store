fe7752af38a65a692ad723f39007dbe4c4f190ab
Carlos <carlos.emmanuel.236@gmail.com>
2025-01-13 06:03:44 -0300

    Adicionando store-with-pins.dto.ts

--- src/stores/dtos/stores-with-pins.dto.ts
+++ src/stores/dtos/stores-with-pins.dto.ts
@@ -1,85 +1,55 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose, Type } from 'class-transformer';
+import { StoreWithDeliveryDto } from './store-with-delivery.dto';
 
 export class StoresWithPinsDto {
-  @Expose()
-  position: {
-    lat: number,
-    lng: number
-  }
-
-  @Expose()
-  title: string;
-}
-
-export class CorreiosDetailsDto {
-  @Expose()
-  sedex: {
-    codProductAgencia: string;
-    price: string;
-    deliveryTime: string;
-    description: string;
-  }
-
-  @Expose()
-  pac: {
-    codProductAgencia: string;
-    price: string;
-    deliveryTime: string;
-    description: string;
-  }
-}
-
-export class MotoboyDetailsDto {
-  @Expose()
-  deliveryTime: string;
-
-  @Expose()
-  price: string;
-
-  @Expose()
-  description: string;
-}
-
-export class StoreWithDeliveryDto {
-  @Expose()
-  name: string;
-
-  @Expose()
-  city: string;
-
-  @Expose()
-  postalCode: string;
-
-  @Expose()
-  type: string;
-
-  @Expose()
-  distance: string;
-
-  @Expose()
-  value: CorreiosDetailsDto | MotoboyDetailsDto;
-}
-
-export class MultipleStoreWithPinsResponseDto {
+  @ApiProperty({
+    description: 'Response status',
+    example: 'success',
+  })
   @Expose()
   status: string;
 
+  @ApiProperty({
+    description: 'The offset of the current page',
+    example: 1,
+  })
   @Expose()
-  offset: number;
+  offset?: number;
 
+  @ApiProperty({
+    description: 'The amount of results you want per page',
+    example: 5,
+  })
   @Expose()
-  limit: number;
+  limit?: number;
 
+  @ApiProperty({
+    description: 'The total number of stores',
+    example: 10,
+  })
   @Expose()
   totalStores: number;
 
+  @ApiProperty({
+    description: 'The total number of pages',
+    example: 2,
+  })
   @Expose()
   totalPages: number;
 
+  @ApiProperty({
+    description: 'List of stores with delivery details',
+    type: [StoreWithDeliveryDto],
+  })
   @Expose()
   @Type(() => StoreWithDeliveryDto)
   stores: StoreWithDeliveryDto[];
 
+  @ApiProperty({
+    description: 'List of pins representing stores on a map',
+    type: [StoresWithPinsDto],
+  })
   @Expose()
   @Type(() => StoresWithPinsDto)
   pins: StoresWithPinsDto[];
