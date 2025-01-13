cfe8eaef75781a8805cdec5cfdcd54ac0cbb4ef4
Carlos <carlos.emmanuel.236@gmail.com>
2025-01-13 05:59:56 -0300

    Adicionando pins.dto.ts

---
+++ src/stores/dtos/pins.dto.ts
@@ -0,0 +1,21 @@
+import { Expose, Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class PinsDto {
+  @ApiProperty({
+    description: 'Position of the store with latitude and longitude',
+    example: { lat: -23.55052, lng: -46.633308 },
+  })
+  @Expose()
+  position: {
+    lat: number;
+    lng: number;
+  };
+
+  @ApiProperty({
+    description: 'Title or name of the store pin',
+    example: 'My Store',
+  })
+  @Expose()
+  title: string;
+}
