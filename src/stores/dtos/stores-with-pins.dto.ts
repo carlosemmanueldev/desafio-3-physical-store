2ea620ed2ff968f59bbad8af1d4dc0bf9b75b31b
Carlos <carlos.emmanuel.236@gmail.com>
2025-01-12 23:38:23 -0300

    Corrigindo dto de stores w/ pins e adicionando novos dtos de delivery

---
+++ src/stores/dtos/stores-with-pins.dto.ts
@@ -0,0 +1,74 @@
+import { Expose, Type } from 'class-transformer';
+
+export class StoresWithPinsDto {
+  @Expose()
+  position: {
+    lat: number,
+    lng: number
+  }
+
+  @Expose()
+  title: string;
+}
+
+export class CorreiosDetailsDto {
+  @Expose()
+  sedex: {
+    codProductAgencia: string;
+    price: string;
+    deliveryTime: string;
+    description: string;
+  }
+
+  @Expose()
+  pac: {
+    codProductAgencia: string;
+    price: string;
+    deliveryTime: string;
+    description: string;
+  }
+}
+
+export class MotoboyDetailsDto {
+  @Expose()
+  deliveryTime: string;
+
+  @Expose()
+  price: string;
+
+  @Expose()
+  description: string;
+}
+
+export class StoreWithDeliveryDto {
+  @Expose()
+  name: string;
+
+  @Expose()
+  city: string;
+
+  @Expose()
+  postalCode: string;
+
+  @Expose()
+  type: string;
+
+  @Expose()
+  distance: string;
+
+  @Expose()
+  value: CorreiosDetailsDto | MotoboyDetailsDto;
+}
+
+export class MultipleStoreWithPinsResponseDto {
+  @Expose()
+  status: string;
+
+  @Expose()
+  @Type(() => StoreWithDeliveryDto)
+  stores: StoreWithDeliveryDto[];
+
+  @Expose()
+  @Type(() => StoresWithPinsDto)
+  pins: StoresWithPinsDto[];
+}
